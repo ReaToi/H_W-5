59a6765efc6f0dd14fd37518a2c0d6a4f5e98f3c
ReaToi <reatoi@mail.ru>
2022-12-23 18:05:37 +0600

    hw6

---
+++ 25-2_beksultan_nurbekov_hw_6.py
@@ -0,0 +1,25 @@
+def names_surnames():
+    
+
+
+
+action = int(input('enter 1 - Count first and last names\nenter 2 - Count all emails\n3 - Read file names'
+                   '\n4 - Count colors\n5 - Exit'))
+
+
+
+
+while action == 5:
+    if action == 1:
+        pass
+    elif action == 2:
+        pass
+    elif action == 3:
+        pass
+    elif action == 4:
+        pass
+    elif action == 5:
+        break
+
+
+
